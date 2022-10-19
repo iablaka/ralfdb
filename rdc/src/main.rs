1635890c019c1da55d68583f6c3c37c2c28a5381
Christophe Leborgne <mactov@iablaka.com>
2022-10-19 22:00:10 +0200

    start coding 1st cmd (use)

--- rdc/src/main.rs
+++ rdc/src/main.rs
@@ -1,10 +1,25 @@
 use std::io;
 use std::io::Write;
+use std::env;
+use std::path::Path;
+
+fn use_command(db: &str) {
+    env::set_var("RALF_DB", db.to_owned() + ".db");
+    let mut path: String = env::var("RALF_PATH").unwrap_or(String::from(".")).to_owned();
+    path.push_str("/");
+    path.push_str(&env::var("RALF_DB").unwrap());
+    if Path::new(&path).exists() {
+        println!("Now using database {}", db);
+    }
+    
+}
+
 
 fn parse_command(cmd: &str) {
     let words: Vec<&str>= cmd.split(" ").collect();
     match words[0].to_lowercase().as_str() {
         "insert" | "select" | "update" | "delete" => println!("to be implemented"),
+        "use" => use_command(&words[1]),
         _ => println!("Unknown command"),
     }
 }
