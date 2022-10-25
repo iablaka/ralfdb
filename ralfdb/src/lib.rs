635787c1274965a3139c3e6ca50539523c4d68ce
Christophe Leborgne <mactov@iablaka.com>
2022-10-25 20:51:50 +0200

    basic select now works

--- ralfdb/src/lib.rs
+++ ralfdb/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+ 
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file: File = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn select(db_name: String, table: String) -> Vec<String> {
+    let file_path: String = db_name + "/" + &table + ".tbl";
+    let mut rows: Vec<String> = Vec::new();
+    if let Ok(lines) = read_lines(&file_path) {
+        for line in lines {
+            if let Ok(record) = line {
+                rows.push(record);
+            }
+        }
     }
+    rows
 }
+
+//TODO a function that returns the column sizes
+//I need to share the path to the db folder to read things inside
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
--- rdc/src/main.rs
+++ rdc/src/main.rs
@@ -2,25 +2,34 @@ use std::io;
 use std::io::Write;
 use std::env;
 use std::path::Path;
+use ralfdb::select;
 
-use ralfdb::add;
+//TODO: format function that prints the result in a table-like format
+//with -, + and |
+
 
 fn use_command(db: &str) {
-    env::set_var("RALF_DB", db.to_owned() + ".db");
     let mut path: String = env::var("RALF_PATH").unwrap_or(String::from(".")).to_owned();
     path.push_str("/");
-    path.push_str(&env::var("RALF_DB").unwrap());
+    path.push_str(db);
+    path.push_str(".db");
     if Path::new(&path).exists() {
+        env::set_var("RALF_DB", path);
         println!("Now using database {}", db);
     }   
+}
+
+fn select_command(_fields: String, table: String) {
+    println!("{:?}", select(env::var("RALF_DB").unwrap(), table));
 }
 
 
 fn parse_command(cmd: &str) {
     let words: Vec<&str>= cmd.split(" ").collect();
     match words[0].to_lowercase().as_str() {
-        "insert" | "select" | "update" | "delete" => println!("to be implemented"),
+        "insert" | "update" | "delete" => println!("to be implemented"),
         "use" => use_command(&words[1]),
+        "select" => select_command(words[1].to_lowercase(), words[3].to_lowercase()),
         _ => println!("Unknown command"),
     }
 }
@@ -33,9 +42,9 @@ fn main() -> io::Result<()> {
         io::stdin().read_line(&mut cmd).expect("Unknown command");
         match cmd.to_lowercase().as_str()  {
             "quit\n" => break,
+            "exit\n" => break,
             _ => parse_command(&cmd.trim()),
         }
     }
-    println!("{}", add(20,22));
     Ok(())
 }
